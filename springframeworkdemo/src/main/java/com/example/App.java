e455466dbcea6af0fb40eb78307e9d162a5c6834
Venkatesh Dwivedi <dwivedivenkatesh98@gmail.com>
2025-06-16 16:23:17 +0530

    scope - singleton and prototype

--- springframeworkdemo/src/main/java/com/example/App.java
+++ springframeworkdemo/src/main/java/com/example/App.java
@@ -9,15 +9,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         
-        ApplicationContext context =new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1= (Alien) context.getBean("alien");
+        ApplicationContext context1 =new ClassPathXmlApplicationContext("spring.xml");
+        Alien obj1= (Alien) context1.getBean("alien");
         // Alien obj=new Alien();
         obj1.code();
+        // ApplicationContext context2 =new ClassPathXmlApplicationContext("spring.xml"); //creates new object when scope is singleton
 
-        Alien obj2= (Alien)context.getBean("alien");
-        obj2.code();
 
-        //calling .getBeans two times does not affect object creation, its still created only one time
+        Alien obj2= (Alien)context1.getBean("alien");//.getbean creates new object when scope gets changed to prototype
+        obj2.code(); 
+        //calling .getBean two times does not affect object creation, its still created only one time
+
 
         
     }
