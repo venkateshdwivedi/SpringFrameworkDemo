769ca0e66b41592edabfc39b3917cdf40bbfcf83
Venkatesh Dwivedi <dwivedivenkatesh98@gmail.com>
2025-06-16 22:28:28 +0530

    manual wiring in spring or injection using ref

--- springframeworkdemo/src/main/java/com/example/Alien.java
+++ springframeworkdemo/src/main/java/com/example/Alien.java
@@ -3,23 +3,35 @@ package com.example;
 public class Alien {
 
     private int age;
+    private Laptop laptop; //only reference created not object
+
     public Alien(){
         System.out.println("alien object created");
     }
 
-    
-    public void code(){
-        System.out.println("coding");
+
+    public Laptop getLaptop() {
+        return laptop;
     }
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+
+ 
 
 
     public int getAge() {
         return age;
     }
-
-
     public void setAge(int age) {
         System.out.println("setter called");
         this.age = age;
     }
+
+     public void code(){
+        System.out.println("coding");
+        laptop.compile();;
+    }
+
 }
---
+++ springframeworkdemo/src/main/java/com/example/Laptop.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Laptop {
+
+    public void compile(){
+        System.out.println("compiling");
+    }
+}
